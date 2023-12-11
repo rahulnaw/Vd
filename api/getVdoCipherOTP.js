e47963cca2354a8c8b140ece300eaf701583b16e
rahulnaw <42603143+rahulnaw@users.noreply.github.com>
2023-12-11 14:23:01 +0530

    Update getVdoCipherOTP.js

--- api/getVdoCipherOTP.js
+++ api/getVdoCipherOTP.js
@@ -2,23 +2,27 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    const { videoId, apiSecret } = req.body;
+  const { videoId, apiSecret } = req.body;
 
-    try {
-        const response = await fetch(`https://dev.vdocipher.com/api/videos/${videoId}/otp`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Apisecret ${apiSecret}`,
-            },
-            body: JSON.stringify({ ttl: 300 }),
-        });
+  try {
+    const response = await fetch(`https://dev.vdocipher.com/api/videos/${videoId}/otp`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Apisecret ${apiSecret}`,
+      },
+      body: JSON.stringify({ ttl: 300 }),
+    });
 
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        console.error('Error fetching VdoCipher OTP:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!response.ok) {
+      throw new Error(`VdoCipher API request failed with status ${response.status}`);
     }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching VdoCipher OTP:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
