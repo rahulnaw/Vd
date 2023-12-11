2f08e62336ecf8a8cdf855b8ef929ee508468451
rahulnaw <42603143+rahulnaw@users.noreply.github.com>
2023-12-11 12:36:04 +0530

    Update getVdocipherOTP.js

--- api/getVdocipherOTP.js
+++ api/getVdocipherOTP.js
@@ -3,33 +3,47 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  const { videoId } = req.query;
-  const vdocipherApiKey = 'your-api-key'; // Replace with your VdoCipher API key
-  const clientSecret = 'your-client-secret'; // Replace with your VdoCipher client secret
+    const vdocipherApiKey = 'rAJ0b4D8snjx8zSLc6MArhRpRnkhCXyJzWQUEf689ppBbxSLMCpsIZyDclNqkftu';
+    const videoId = '1fdf6a21d9594c5ebc2e4aef345af8de';
 
-  try {
-    const response = await fetch(`https://dev.vdocipher.com/api/videos/${videoId}/otp`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': `Apisecret ${clientSecret}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        ttl: 300,
-      }),
-    });
+    try {
+        const response = await fetch(`https://dev.vdocipher.com/api/videos/${videoId}/otp`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Apisecret ${vdocipherApiKey}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                ttl: 300
+            })
+        });
 
-    const data = await response.json();
+        // Check if the response is successful (status 2xx)
+        if (!response.ok) {
+            throw new Error(`Failed to get OTP. Status: ${response.status}`);
+        }
 
-    // Set CORS headers to allow requests from any origin
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        const data = await response.json();
 
-    res.json(data);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+        // Log the complete response from Vdocipher API
+        console.log('Vdocipher API Response:', data);
+
+        // Set CORS headers to allow requests from any origin
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+        // Check if the response contains valid JSON
+        if (!data || !data.otp || !data.playbackInfo) {
+            throw new Error('Invalid response from Vdocipher API');
+        }
+
+        res.json(data);
+    } catch (error) {
+        console.error('Error:', error);
+
+        // Send an appropriate error response
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
