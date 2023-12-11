b4aa8167eaa3582fa219162b8337722dd7cb4896
rahulnaw <42603143+rahulnaw@users.noreply.github.com>
2023-12-11 14:08:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Replace "YOUR_API_SECRET_KEY" and "VIDEO_ID" with your VdoCipher API secret key and video ID
-    const apiSecretKey = "YOUR_API_SECRET_KEY";
-    const videoId = "VIDEO_ID";
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
 
-    // Step 1: HTTP request to obtain OTP
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Apisecret ${apiSecretKey}`
-        },
-        body: JSON.stringify({ ttl: 300 }),
-    };
+app.use(express.json());
 
-    fetch(`https://dev.vdocipher.com/api/videos/${videoId}/otp`, requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            // Step 2: Use OTP and playbackInfo in embed code
-            const otp = data.otp;
-            const playbackInfo = data.playbackInfo;
+app.post('/getVdoCipherOTP', async (req, res) => {
+  try {
+    const response = await fetch(`https://dev.vdocipher.com/api/videos/${req.body.videoId}/otp`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Apisecret ${req.body.apiSecret}`
+      },
+      body: JSON.stringify({ ttl: 300 }),
+    });
 
-            const iframe = document.createElement("iframe");
-            iframe.src = `https://player.vdocipher.com/v2/?otp=${otp}&playbackInfo=${playbackInfo}`;
-            iframe.style.border = "0";
-            iframe.style.width = "720px";
-            iframe.style.height = "405px";
-            iframe.allow = "encrypted-media";
-            iframe.allowFullscreen = true;
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching VdoCipher OTP:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-            document.getElementById("vdoCipherPlayer").appendChild(iframe);
-        })
-        .catch(error => console.error("Error fetching VdoCipher OTP:", error));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
